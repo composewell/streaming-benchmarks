b9bdb60142de4cef422d20fa8763142215300d5a
Harendra Kumar <harendra@composewell.com>
2021-07-04 22:52:12 +0530

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,94 @@
+# CAUTION! a spelling mistake in arg string is ignored silently.
+#
+# nix-shell --argstr c2nix "--flag examples-sdl"
+
+# To use ghc-8.6.5
+# nix-shell --argstr compiler "ghc865"
+
+{
+  nixpkgs ?
+    import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz)
+        {}
+, compiler ? "default"
+, c2nix ? "" # cabal2nix CLI options
+# TODO
+#, sources ? [] # e.g. [./. ./benchmark]
+#, hdeps ? [] # e.g. [time, mtl]
+#, deps ? [] # e.g. [SDL2]
+}:
+let haskellPackages =
+        if compiler == "default"
+        then nixpkgs.haskellPackages
+        else nixpkgs.haskell.packages.${compiler};
+
+    # we can possibly avoid adding our package to HaskellPackages like
+    # in the case of nix-shell for a single package?
+    mkPackage = super: pkg: path: opts: inShell:
+                let orig = super.callCabal2nixWithOptions pkg path opts {};
+                 in if inShell
+                    # Avoid copying the source directory to nix store by using
+                    # src = null.
+                    then orig.overrideAttrs (oldAttrs: { src = null; })
+                    else orig;
+
+    flags = "--benchmark" + " " + c2nix;
+
+    mkHaskellPackages = inShell:
+        haskellPackages.override {
+            # We could disbale doCheck on all like this, but it would make the
+            # whole world rebuild, we can't use the binary cache
+            #packageSetConfig = self: super: {
+            #    mkDerivation = drv: super.mkDerivation (drv // {
+            #        doCheck = false;
+            #    });
+            #};
+            overrides = self: super:
+                with nixpkgs.haskell.lib;
+                {
+                    streaming-benchmarks =
+                        mkPackage super "streaming-benchmarks"
+                            ./. flags inShell;
+
+                    streamly =
+                      nixpkgs.haskell.lib.overrideCabal
+                        (super.callHackageDirect
+                          { pkg = "streamly";
+                            ver = "0.8.0";
+                            sha256 = "0vy2lkljizlhpbpbybmg9jcmj2g4s1aaqd2dzy5c0y0n4rgwxask";
+                          } {})
+                        (old:
+                          { librarySystemDepends =
+                              if builtins.currentSystem == "x86_64-darwin"
+                              then [nixpkgs.darwin.apple_sdk.frameworks.Cocoa]
+                              else [];
+                            enableLibraryProfiling = false;
+                            doHaddock = false;
+                          });
+
+                    fusion-plugin =
+                      super.callHackageDirect
+                        { pkg = "fusion-plugin";
+                          ver = "0.2.3";
+                          sha256 = "073wbhdxj1sh5160blaihbzkkhabs8s71pqhag16lvmgbb7a3hla";
+                        } {};
+                };
+        };
+
+    hspkgs = mkHaskellPackages true;
+
+    shell = hspkgs.shellFor {
+        packages = p:
+          [ p.streaming-benchmarks
+          ];
+        doBenchmark = true;
+        shellHook = ''
+          export CABAL_DIR="$(pwd)/.cabal.nix"
+          if test -n "$PS_SHELL"
+          then
+            export PS1="$PS_SHELL\[$bldred\](nix)\[$txtrst\] "
+          fi
+        '';
+    };
+in if nixpkgs.lib.inNixShell
+   then shell
+   else (mkHaskellPackages false).streaming-benchmarks
