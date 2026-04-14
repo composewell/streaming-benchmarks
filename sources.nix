92ac1629b6fbccf06098e826c0d73837e8d818e4
Harendra Kumar <harendra@composewell.com>
2026-04-14 16:21:28 +0530

    Use the latest fusion-plugin for ghc-9.14 support

--- sources.nix
+++ sources.nix
@@ -7,6 +7,8 @@ layers = [
   #streaming-benchmarks = localOpts ./.
     #[]
     #["--flags streaming"];
+  fusion-plugin =
+    gh "composewell" "fusion-plugin" "15c0ad50a235a35e85d03cced9ad0f3938565e5a";
 }
 ];
 }
